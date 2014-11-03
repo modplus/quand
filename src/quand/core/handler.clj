3a4d45ee2e06415857adb28332194686ee7ac92f
Bryan Maass <bryan.maass@gmail.com>
2014-11-02 18:21:42 -0800

    fix handler

--- src/quand/core/handler.clj
+++ src/quand/core/handler.clj
@@ -3,14 +3,11 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [quand.pages.landing :as landing]
-            [cider.nrepl :as nrepl]
-            [ring.adapter.jetty :as jetty]))
+            [cider.nrepl :as nrepl]))
 
 (defn v [req]
   (def *r req)
-  (str
-   "session token: " 
-   (-> req :cookies (get "ring-session") :value)))
+  "hello")
 
 (defroutes app-routes
   (GET "/" [] landing/page)
@@ -19,7 +16,3 @@
 
 (def app
   (wrap-defaults app-routes site-defaults))
-
-(defn -main [& args]
-  (nrepl/start-server )
-  (jetty/run-jetty app {:port 10501}))
